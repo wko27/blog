6da92432c35f87b1c35a1407aea94336dd79876a
wko27 <wing.hong.a.ko@gmail.com>
2017-08-17 16:21:09 -0400

    fixes

--- assets/comment.js
+++ assets/comment.js
@@ -1,12 +1,10 @@
 // Submit a comment for the given post
-function submitComment(post) {
+function submitComment(postid) {
   var name = document.getElementById("name").value || "anonymous";
   var comment = document.getElementById("comment").value;
   var to = "wko27code@gmail.com";
-  console.log("post is: " + post);
-  var subject = "[blog-comment]:" + post;
-  // base64 encode message body
-  var body = btoa(name + ":\n" + comment);
+  var subject = encodeURIComponent("[blog-comment]:" + postid);
+  var body = encodeURIComponent(name + ":\n" + comment);
   var gmail = "mailto:" + to + "?subject=" + subject + "&body=" + body;
   window.open(gmail, '_blank');
 }
@@ -17,10 +15,9 @@ window.onload = function() {
   if (!button) {
     return;
   }
-      
+  
   button.addEventListener("click", function() {
-    var url = button.getAttribute("data-pageurl");
-    console.log("url is: " + url);
-    submitComment(url);
+    var postid = button.getAttribute("data-postid");
+    submitComment(postid);
   }, false);
 };
--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@
 # Do some validation checks
 for post in $( git status | grep modified:.*_posts | sed 's/modified://g' | tr -d '[:space:]' ); do
     echo "-- Validating post: $post"
-    expectedTitle=$( echo "${post%.*}" | cut -d '-' -f 4- )
-    actualTitle=$( cat "$post" | grep "title" | head -n 1 | sed 's/title: //' )
+    expectedTitle=$( echo "${post%.*}" | cut -d '-' -f 4- | sed 's/[^a-z]//g' )
+    actualTitle=$( cat "$post" | grep "title" | head -n 1 | sed 's/title: //' | sed 's/[^a-z]//g' )
     if [[ "$expectedTitle" != "$actualTitle" ]]; then
 	echo "Expected title to be $expectedTitle but found $actualTitle"
 	exit 1
